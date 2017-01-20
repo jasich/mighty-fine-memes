46cc2a4c7b8b867ee1faa2bf63ffe72bfd098475
Jason Sich <jasich@gmail.com>
2017-01-19 21:34:24 -0500

    WIP on text

--- src/cljs/memegen_ui/events.cljs
+++ src/cljs/memegen_ui/events.cljs
@@ -8,6 +8,7 @@
             [memegen-ui.lib.search :as search]
             [memegen-ui.lib.ux :as ux]
             [dandy-roll.core :as dr]
+            [dandy-roll.draw :as draw]
             [dandy-roll.canvas :refer [data-url]]
             [dandy-roll.promise :refer [then]]))
 
@@ -151,18 +152,72 @@
  (fn [db _]
    (assoc db :editor-message "")))
 
+(def sizes {:big {:count 10
+                  :size 100}
+            :big1 {:count 20
+                   :size 55}
+            :med {:count 30
+                  :size 35}
+            :small1 {:count 40
+                     :size 27}
+            :small{:count 50
+                   :size 25}})
+
+(def rate-of-change (/ (- (get-in sizes [:small :size]) (get-in sizes [:big :size]))
+                       (- (get-in sizes [:small :count]) (get-in sizes [:big :count]))))
+(def y-intercept (- (get-in sizes [:big :size]) (* rate-of-change (get-in sizes [:big :count]))))
+
+(def magic-number 1031.125447)
+;; 100 = rate-of-change * 10 + b
+
+(defn get-font-size [char-count]
+  (let [suggested-size (+ 3 (/ 1171.037212 (+ char-count 1.701793604)))]
+    (if (> suggested-size 100)
+      100
+      suggested-size)))
+
+(defn center-x [canvas drawable]
+  (/ (- (.-width canvas) (draw/width drawable canvas)) 2))
+
+(defn needs-break [words]
+  (let [word-count (count words)]
+    (> word-count 5)))
+
+(defn pick-a-size [top-text bottom-text]
+  (let [top-count (count top-text)
+        bot-count (count bottom-text)]
+    (if (> top-count bot-count)
+      (int (get-font-size top-count))
+      (int (get-font-size bot-count)))))
+
+
 (re-frame/reg-event-db
  :update-meme-image
  (fn [db _]
    (let [meme-url (:meme-url db)
          top-text (str/upper-case (:top-text db))
+         top-text-words (str/split top-text #" ")
+         top-text-break? (needs-break top-text-words)
          bottom-text (str/upper-case (:bottom-text db))
-         stroked-top (dr/center-top {:strokeStyle "black" :lineWidth 3})
-         stroked-bottom (dr/center-bottom {:strokeStyle "black" :lineWidth 3})]
-     (dr/watermark meme-url
-                   (dr/with-text top-text 72 "Impact" "#fff" stroked-top)
-                   (dr/with-text bottom-text 62 "Impact" "#fff" stroked-bottom)
-                   (dr/to-data-url #(re-frame/dispatch [:update-meme-url %]))))
+         stroked-top (dr/center-top {:strokeStyle "black" :lineWidth 2})
+         stroked-bottom (dr/center-bottom {:strokeStyle "black" :lineWidth 2})
+         top-font-size (int (get-font-size (count top-text)))
+         second-line (dr/drawer center-x #(identity (+ 30 top-font-size)))]
+     (if top-text-break?
+       (let [break-point (.round js/Math (/ (count top-text-words) 2))
+             top-line (str/join " " (take break-point top-text-words))
+             bot-line (str/join " " (drop break-point top-text-words))
+             font-size (pick-a-size top-line bot-line)]
+         (dr/watermark meme-url
+                       (dr/with-text top-line font-size "Impact" "#fff" stroked-top)
+                       (dr/with-text bot-line font-size "Impact" "#fff"
+                         (second-line {:strokeStyle "black" :lineWidth 2}))
+                       (dr/with-text bottom-text 27 "Impact" "#fff" stroked-bottom)
+                       (dr/to-data-url #(re-frame/dispatch [:update-meme-url %]))))
+       (dr/watermark meme-url
+                       (dr/with-text top-text top-font-size "Impact" "#fff" stroked-top)
+                       (dr/with-text bottom-text 27 "Impact" "#fff" stroked-bottom)
+                       (dr/to-data-url #(re-frame/dispatch [:update-meme-url %])))))
      db))
 
 
